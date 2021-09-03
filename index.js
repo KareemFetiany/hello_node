c24bf8d74de0f037f3b04a9a8d66b6f21af4b673
Kamila <kareemaly101@gmail.com>
2021-09-04 01:36:01 +0200

    Create Basic Server

--- index.js
+++ index.js
@@ -5,12 +5,5 @@ app.get('/', function(req, res){
    res.send("Hello world!");
 });
 
-app.get('/hello', function(req, res){
-    res.send("Hello World!");
- });
- 
- app.post('/hello', function(req, res){
-    res.send("You just called the post method at '/hello'!\n");
- });
 
 app.listen(3000);
